db313ab159304d32a5c2e6c7993f2274fd7e9471
ANKIT RAJ <ankitraj12.02.2005@gmail.com>
2026-07-03 15:15:14 +0530

    Time: 0 ms (100.00%) | Memory: 43.1 MB (48.98%) - LeetSync

--- 92-reverse-linked-list-ii/reverse-linked-list-ii.java
+++ 92-reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -10,24 +10,27 @@
  */
 class Solution {
     public ListNode reverseBetween(ListNode head, int left, int right) {
-        ListNode dummy=new ListNode(0);
-        dummy.next=head;
-        ListNode leftpre=dummy;
-        ListNode currnode=head;
-        for(int i=0;i<left-1;i++){
-            leftpre=leftpre.next;
-            currnode=currnode.next;
-        }
-        ListNode sublist=currnode;
-        ListNode prenode=null;
-        for(int i=0;i<right-left+1;i++){
-            ListNode nextnode=currnode.next;
-            currnode.next=prenode;
-            prenode=currnode;
-            currnode=nextnode;
-        }
-        leftpre.next=prenode;
-        sublist.next=currnode;
-        return dummy.next;
+       ListNode dummy =new ListNode(0);
+       dummy.next=head;
+       ListNode leftPre=dummy;
+       ListNode currnode=head;
+       for(int i=0;i<left-1;i++){
+        leftPre=leftPre.next;
+        currnode=currnode.next;
+       }
+       ListNode prenode=null;
+       ListNode sublisthead=currnode;
+       for(int i=0;i<right-left+1;i++){
+        ListNode nextnode=currnode.next;
+        currnode.next=prenode;
+        prenode=currnode;
+        currnode=nextnode;
+       }
+       leftPre.next=prenode;
+       sublisthead.next=currnode;
+       return dummy.next;
     }
 }
+    
+
+
