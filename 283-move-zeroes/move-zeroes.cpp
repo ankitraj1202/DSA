4b4983f4aa58cb3ed1a8b3c446b3c3d9626bc299
ANKIT RAJ <ankitraj12.02.2005@gmail.com>
2026-07-31 15:09:16 +0530

    Time: 0 ms (100.00%) | Memory: 24 MB (19.77%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -1,18 +1,13 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        vector<int>temp;
-        for(int i=0;i<nums.size();i++){
-            if(nums[i] != 0){
-                temp.push_back(nums[i]);
-            }
+      int mz=0;
+      int n=nums.size();
+      for(int i=0;i<n;i++){
+        if(nums[i] !=0){
+            swap(nums[mz],nums[i]);
+            mz++;
         }
-        int nt=temp.size();
-        for(int i=0;i<nt;i++){
-            nums[i]=temp[i];
-        }
-        for(int i=nt;i<nums.size();i++){
-            nums[i]=0;
-        }
+      }  
     }
 };
